d10e6f57ec86d249be7a71e5207401571af923d0
melayer <melayer.chetanyeshi@gmail.com>
2018-12-18 15:41:08 +0530

    calculation

---
+++ js/three.js
@@ -0,0 +1,15 @@
+//Addition
+var sum=10 + 10;  
+console.log(sum)
+
+//Subtraction
+var difference=109 - 10;
+console.log(difference)
+
+//Multiplication
+var product=7 * 7;
+console.log(product)
+
+//Division
+var quotient=97 / 7;
+console.log(quotient)
