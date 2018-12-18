4a5a51b1b0769f00b78795d50b6e4968c44af0b5
melayer <melayer.chetanyeshi@gmail.com>
2018-12-18 16:52:34 +0530

    quote-concat

---
+++ js/five.js
@@ -0,0 +1,12 @@
+//concatinating String
+var myStr="This is Start. "+"This is End."
+console.log(myStr);
+
+var ourStr="This is the first sentence.;"
+ourStr +="This is the last sentence.";
+console.log(ourStr);
+
+//concatinate with variables
+var myName="Viraj";
+var myStr="My name is "+myName+" and I am good.";
+console.log(myStr);
