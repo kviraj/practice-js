d272b4d2e8d1aab6906657d176bf668dae82cb01
melayer <melayer.chetanyeshi@gmail.com>
2018-12-22 14:50:50 +0530

    lst-nm

---
+++ js/seven.js
@@ -0,0 +1,9 @@
+var FirstName="Ram";
+
+ lastLetterOfFirstName=FirstName[FirstName.length-1];
+
+var LastName="Verma";
+
+var firstLetterOfLastName=LastName[LastName.length-1];
+
+console.log(firstLetterOfLastName);
