2b0b1e5d14fe2fa86acb0c1fc4f9424602168574
melayer <melayer.chetanyeshi@gmail.com>
2018-12-22 16:26:35 +0530

    array(push&pop)

---
+++ js/12.js
@@ -0,0 +1,11 @@
+var ourArray=["John","stuart","dog"];
+ourArray.push("dublin","moscow");
+var removedFromourArray=ourArray.pop();
+
+var myArray=["chef",99,"bull",34];
+var removedFrommyArray=myArray.pop();
+myArray.push("cat",4);
+
+console.log(ourArray);
+
+console.log(myArray);
