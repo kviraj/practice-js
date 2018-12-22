62fa38fe753dc9da6b411a5f1dc45f99a675ffeb
melayer <melayer.chetanyeshi@gmail.com>
2018-12-22 18:02:18 +0530

    push-pop-shift-unshift

--- js/12.js
+++ js/12.js
@@ -1,10 +1,16 @@
 var ourArray=["John","stuart","dog"];
 ourArray.push("dublin","moscow");
 var removedFromourArray=ourArray.pop();
+var removedFromourArray=ourArray.shift();
+ourArray.unshift("sanket");
+
 
 var myArray=["chef",99,"bull",34];
 var removedFrommyArray=myArray.pop();
 myArray.push("cat",4);
+var removedFromourArray=myArray.shift();
+myArray.unshift("viraj");
+
 
 console.log(ourArray);
 
