caa35d916f9c2e930652d7e15ce8bf0ed99d8737
melayer <melayer.chetanyeshi@gmail.com>
2018-12-22 18:24:02 +0530

    reusable-function

---
+++ js/13.js
@@ -0,0 +1,19 @@
+function ourReusableFunction()
+{
+    console.log("Hello World");
+
+}
+ ourReusableFunction()
+ ourReusableFunction() 
+ ourReusableFunction()
+  ourReusableFunction()
+
+
+  function myReusableFunction()
+  {
+    console.log("Panda");
+  }
+   
+  myReusableFunction()
+  myReusableFunction()
+  myReusableFunction()
