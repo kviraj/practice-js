b994b7576ffdbf57a4471662740ec7ed82aab49c
melayer <melayer.chetanyeshi@gmail.com>
2018-12-22 18:32:54 +0530

    locally declared

---
+++ js/14.js
@@ -0,0 +1,12 @@
+function ourFunctionWithArgs(a,b)
+{ 
+    console.log(a-b);
+}
+ourFunctionWithArgs(10,5);
+
+
+function yourFunctionWithArgs(a,b)
+{ 
+    console.log(a+b);
+}
+yourFunctionWithArgs(10,5);
