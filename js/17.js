72c810cc1bcf080687f5a5977978e43827099025
melayer <melayer.chetanyeshi@gmail.com>
2018-12-24 12:41:30 +0530

    function calculation

---
+++ js/17.js
@@ -0,0 +1,8 @@
+var outWear="Shirt";
+function myOutfit()
+{
+    var outWear="cap";
+    return outWear;
+}
+console.log(myOutfit());
+console.log(outWear);
---
+++ js/18.js
@@ -0,0 +1,13 @@
+function sub(num)
+{
+    return num -6;
+}
+
+console.log(sub(10));
+
+function mul(num)
+{
+    return num *4;
+}
+
+console.log(mul(5));
